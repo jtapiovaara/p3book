4e106907b0b86832cfa5c21daab590b4e8944d16
jta <jtapiovaara@gmail.com>
2020-01-23 20:52:09 +0200

    First version of three table ORM playground presenting all the books that my late father decorated.

---
+++ books/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'books'
+
+urlpatterns = [
+    path('books/', views.BookList.as_view(), name='books'),
+    path('authors/', views.AuthorList.as_view(), name='authors'),
+    path('publishers/', views.PublisherList.as_view(), name='publishers'),
+    # path('book/<publisher>', views.ListView.as_view(), name='booksbypublisher'),
+    # path('author/<int:pk>', views.ListView.as_view(), name='authorsbooks'),
+    path('book/<int:pk>', views.BookDetail.as_view(), name='bookdetail'),
+    path('authors/<int:pk>', views.AuthorDetail, name='authordetail'),
+    path('publishers/<int:pk>', views.publisherdetail, name='publisherdetail'),
+]
---
+++ books/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from books.models import Publisher, Book, Author
+
+
+# Create your views here.
+
+class BookList(ListView):
+    model = Book
+    context_object_name = 'books_by_tapsa'
+
+
+class BookDetail(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    pass
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Book.objects.filter()
+
+
+class AuthorList(ListView):
+    model = Author
+    context_object_name = 'authors_with_tapsa_twix'
+
+
+def AuthorDetail(request, pk):
+    author_yx = Author.objects.get(pk=pk)
+    # authors_books = Book.objects.filter(authors=author_yx.authors)
+    authors_books = Book.objects.all()
+
+    context = {
+        'author_yx': author_yx,
+        'authors_books': authors_books
+    }
+    return render(request, 'books/author_detail.html', context)
+
+
+class PublisherList(ListView):
+    model = Publisher
+    context_object_name = 'my_favorite_publishers'
+
+
+def publisherdetail(request, pk):
+
+    publisher_yx = Publisher.objects.get(pk=pk)
+    publishers_books = Book.objects.filter(publisher_id=pk)
+
+    context = {
+        'publisher_yx': publisher_yx,
+        'publishers_books': publishers_books
+    }
+    return render(request, 'books/publisher_detail.html', context)
+
+
+
+
